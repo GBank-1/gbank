69c842d0c92dd84471dc193dc721fa970bc2c781
Helis Freitas <dfhelis@gmail.com>
2021-11-05 15:11:45 -0300

    Update ClienteDTO.java

--- src/main/java/br/gbank/gbank/dto/ClienteDTO.java
+++ src/main/java/br/gbank/gbank/dto/ClienteDTO.java
@@ -5,6 +5,8 @@ import br.gbank.gbank.model.Cliente;
 public class ClienteDTO {
 	private String nome;
 	private String email;
+	private String cpf;
+	private String telefone;
 
 
 	public String getNome() {
@@ -22,7 +24,21 @@ public class ClienteDTO {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public void getCpf() {
+		return cpf;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	
 	public static ClienteDTO toClienteDTO(Cliente cliente) {
 		ClienteDTO clienteDTO = new ClienteDTO();
 		clienteDTO.setNome(cliente.getDadosPessoais().getNome());
