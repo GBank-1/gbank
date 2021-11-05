a8cb707e5b4c1842495fedf9a94e04b669c5d623
Helis Freitas <dfhelis@gmail.com>
2021-11-05 14:55:57 -0300

    Update ContaResource.java
    Fix merge erros

--- src/main/java/br/gbank/gbank/resources/ContaResource.java
+++ src/main/java/br/gbank/gbank/resources/ContaResource.java
@@ -1,15 +1,13 @@
 package br.gbank.gbank.resources;
 
-import br.gbank.gbank.dto.ContaDTO;
-import br.gbank.gbank.service.ContaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControler;
+import br.gbank.gbank.dto.ContaDTO;
 import br.gbank.gbank.model.Conta;
 import br.gbank.gbank.service.ContaService;
 import io.swagger.annotations.Api;
